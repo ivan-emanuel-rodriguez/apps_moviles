7dd1069fdbfea06db580cc52992abcd98884af0d
ivancio <ivancio87@gmail.com>
2021-09-22 19:58:30 -0300

    Carrito de compras implementado

--- Practica 2/js/index.js
+++ Practica 2/js/index.js
@@ -1,6 +1,4 @@
-//FUNCIONA CON PRECIOS DISTINTOS PERO SE REPITE MUCHAS VECES LA MISMA LOGICA DE CODIGO
-
-$(document).on('click', '#art1', function () {
+$(document).on('click', 'article', function () {
     var carrito = $('#preciocarrito').text();
       if (this.value != "seleccionado") {
         $('img', this).css('opacity', .75);
@@ -8,9 +6,62 @@ $(document).on('click', '#art1', function () {
         $('.artista',this).css('color', "white");
         this.value = "seleccionado";
         //Cargar carrito
-        var precio1 = $('#precio1').text();
-        var carrito = parseFloat(carrito) + parseFloat(precio1);
-        $('#preciocarrito').text(carrito);
+        var art = $('img', this).attr("alt");
+        if (art == "duki"){
+          var precio= $('#precio1').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "daddy"){
+          var precio= $('#precio2').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "badbunny"){
+          var precio= $('#precio3').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "shakira"){
+          var precio= $('#precio4').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "luisfonsi"){
+          var precio= $('#precio5').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "thalia"){
+          var precio= $('#precio6').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "sodaestereo"){
+          var precio= $('#precio7').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "lospiojos"){
+          var precio= $('#precio8').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "indiosolari"){
+          var precio= $('#precio9').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "tiesto"){
+          var precio= $('#precio10').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else{
+          var precio= $('#precio11').text();
+          var carrito = parseFloat(carrito) + parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
       }
       else {
         $('.artista .imagen img', this).css('opacity', 1);
@@ -18,57 +69,65 @@ $(document).on('click', '#art1', function () {
         $('.artista',this).css('color', "black");
         this.value = ""    
         //Descargar carrito
-        var precio1 = $('#precio1').text();
-        var carrito = parseFloat(carrito) - parseFloat(precio1);
-        $('#preciocarrito').text(carrito);
+        var art = $('img', this).attr("alt");
+        if (art == "duki"){
+          var precio= $('#precio1').text();
+          var carrito = parseFloat(carrito) - parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+        else if(art == "daddy"){
+          var precio= $('#precio2').text();
+          var carrito = parseFloat(carrito) - parseFloat(precio);
+          $('#preciocarrito').text(carrito);
+        }
+          else if(art == "badbunny"){
+            var precio= $('#precio3').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "shakira"){
+            var precio= $('#precio4').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "luisfonsi"){
+            var precio= $('#precio5').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "thalia"){
+            var precio= $('#precio6').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "sodaestereo"){
+            var precio= $('#precio7').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "lospiojos"){
+            var precio= $('#precio8').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "indiosolari"){
+            var precio= $('#precio9').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else if(art == "tiesto"){
+            var precio= $('#precio10').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
+          else{
+            var precio= $('#precio11').text();
+            var carrito = parseFloat(carrito) - parseFloat(precio);
+            $('#preciocarrito').text(carrito);
+          }
       }
 });
 
-$(document).on('click', '#art2', function () {
-  var carrito = $('#preciocarrito').text();
-  if (this.value != "seleccionado") {
-    $('img', this).css('opacity', .75);
-    $('.artista',this).css('background-color', "rgb(80, 80, 80)");
-    $('.artista',this).css('color', "white");
-    this.value = "seleccionado";
-    //Cargar carrito
-    var precio2 = $('#precio2').text();
-    var carrito = parseFloat(carrito) + parseFloat(precio2);
-    $('#preciocarrito').text(carrito);
-  }
-  else {
-    $('.artista .imagen img', this).css('opacity', 1);
-    $('.artista',this).css('background-color', "");
-    $('.artista',this).css('color', "black");
-    this.value = ""
-    var precio2 = $('#precio2').text();
-    var carrito = parseFloat(carrito) - parseFloat(precio2);
-    $('#preciocarrito').text(carrito);    
-  }
-});
-
-$(document).on('click', '#art3', function () {
-  var carrito = $('#preciocarrito').text();
-  if (this.value != "seleccionado") {
-    $('img', this).css('opacity', .75);
-    $('.artista',this).css('background-color', "rgb(80, 80, 80)");
-    $('.artista',this).css('color', "white");
-    this.value = "seleccionado";
-    //Cargar carrito
-    var precio3 = $('#precio3').text();
-    var carrito = parseFloat(carrito) + parseFloat(precio3);
-    $('#preciocarrito').text(carrito);
-  }
-  else {
-    $('.artista .imagen img', this).css('opacity', 1);
-    $('.artista',this).css('background-color', "");
-    $('.artista',this).css('color', "black");
-    this.value = ""
-    var precio3 = $('#precio3').text();
-    var carrito = parseFloat(carrito) - parseFloat(precio3);
-    $('#preciocarrito').text(carrito);    
-  }
-});
 
 $(document).on('click', '#logo', function () {
     window.location.href='/html/index.html'
