6527e55867ebbb66c2cbdc6466939f92ece6757d
Danieltq04 <daniel.tq04@gmail.com>
2021-09-26 17:47:19 -0300

    carrito en todas las vistas

--- Practica 2/js/latino.js
+++ Practica 2/js/latino.js
@@ -16,7 +16,7 @@ function CargarArtistas() {
             console.log(artista);
             if(artista.categoria == "latino"){
                 var card = `
-                <article>
+                <article id="${i}">
                     <div class="artista">
                         <div class="imagen">
                             <img class="foto" src="../img/${artista.imagen}" alt="shakira" />
@@ -25,9 +25,9 @@ function CargarArtistas() {
                             <p>
                                 ${artista.artista}
                             </p>
-                            <p>
-                                . Precio: ${artista.precio}
-                            </p>
+                            <div class="valor" id="artista${i}" value="${artista.precio}">
+                                <p>. Precio: ${artista.precio}</p>
+                            </div>
                         </div>
                     </div>
                 </article>
@@ -38,4 +38,53 @@ function CargarArtistas() {
         })
     });
 }
-
+/*
+<article id="4">
+                        <div class="artista">
+                            <div class="imagen">
+                                <img class="foto" src="../img/shakira.jpg" alt="shakira" />
+                            </div>
+                            <div class="descripcion">
+                                <p>
+                                    Shakira: Wakawaka.
+                                    Precio $
+                                    <div class="valor" id="artista4" value="1318">
+                                        <p id="precio4">1318</p>
+                                    </div>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    <article id="5">
+                        <div class="artista">
+                            <div class="imagen">
+                                <img class="foto" src="../img/luisfonsi.jpg" alt="luisfonsi" />
+                            </div>
+                            <div class="descripcion">
+                                <p>
+                                    Luis Fonsi: No me doy por vencido.
+                                    Precio $
+                                    <div class="valor" id="artista5" value="2009">
+                                        <p id="precio5">2009</p>
+                                    </div>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    <article id="6">
+                        <div class="artista">
+                            <div class="imagen">
+                                <img class="foto" src="../img/thalia.jpg" alt="thalia" />
+                            </div>
+                            <div class="descripcion">
+                                <p>
+                                    Thalia: Equivocada.
+                                    Precio $
+                                    <div class="valor" id="artista6" value="2144">
+                                        <p id="precio6">2144</p>
+                                    </div>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    */
